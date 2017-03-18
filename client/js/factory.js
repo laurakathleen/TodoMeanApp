c7c5bf7a3a98c1e86b354515ef9e9788b34b7047
laurakathleen <laurakwalley@gmail.com>
2017-03-18 13:50:40 -0700

    adds a service to interact with express rest endpoints

---
+++ client/js/factory.js
@@ -0,0 +1,24 @@
+//interacts with Express REST endpoints
+
+todoApp.factory('todosFactory', function($http){
+	var urlBase = '/api/todos';
+	var _todoService = {};
+
+	_todoService.getTodos = function(){
+		return $http.get(urlBase);
+	};
+
+	_todoService.saveTodo = function(todo){
+		return $http.post(urlBase, todo);
+	};
+
+	_todoService.updateTodo = function(todo){
+		return $http.put(urlBase, todo);
+	};
+
+	_todoService.deleteTodo = function(todo){
+		return $http.delete(urlBase + '/' + id);
+	};
+
+	return _todoService;
+});
